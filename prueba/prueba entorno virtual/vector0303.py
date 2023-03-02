2eb50197043d23fb42f1bb58d8eee5231464953a
Juan Diego Arias <juand.ariasi@uqvirtual.edu.co>
2023-03-02 17:21:12 -0500

    commit defino vector

---
+++ prueba/prueba entorno virtual/vector0303.py
@@ -0,0 +1,39 @@
+class vector:
+    def __init__(self,x,y,z):
+        self.x = x 
+        self.y = y
+        self.z = z
+        self.magnitud = (self.x**2+self.y**2+self.z**2)**0.5
+ #Notación para imprimir respuesta
+    def __str__(self):
+        respuesta = "["+str(self.x)+" , "+str(self.y)+" , "+str(self.z)+"]"
+        return(respuesta)
+#suma
+    def __add__(self,otro):
+        x = self.x + otro.x
+        y = self.y + otro.y
+        z = self.z + otro.z
+        return(vector(x,y,z))
+#resta
+    def __sub__(self,otro):
+        x = self.x - otro.x
+        y = self.y - otro.y
+        z = self.z - otro.z
+        return(vector(x,y,z))
+#producto escalar 
+    def __mul__(self,otro):
+        x = self.x*otro.x
+        y = self.y*otro.y
+        z = self.z*otro.z
+        return(x+y+z)
+
+
+v1 = vector(1,2,3)
+v2 = vector(2,6,9)
+v3 = v1+v2
+v4 = v1*v2
+print("suma:", v3)
+print("producto punto:", v4)
+
+
+
