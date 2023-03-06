65faeeaa880a19bb8ae4f990e4d20ff1c7471df3
Juan Diego Arias <juand.ariasi@uqvirtual.edu.co>
2023-03-06 18:01:20 -0500

    comitt_operaciones_con_matrices

--- Archivos clases/Archibox/vector0303.py
+++ Archivos clases/Archibox/vector0303.py
@@ -35,23 +35,74 @@ class vector:
              return(resultadoescalar)
         else:
             return(vector(otro*self.x, otro*self.y, otro*self.z))
+            
+        
+ #definir matrices 2x2       
+
+class matriz:
+    def __init__ (self, matriz):
+        self.matriz = matriz 
+
+    def __str__(self):
+        respuesta = "| "+str(self.matriz[0][0])+"  "+str(self.matriz[0][1])+" |\n| "+str(self.matriz[1][0])+"  "+str(self.matriz[1][1])+" |"
+        return(respuesta)
+    
+    #Defino suma
+    def __add__(self, otra):
+        nueva_matriz = [ [self.matriz[0][0]+otra.matriz[0][0], self.matriz[0][1]+otra.matriz[0][1]], 
+                        [self.matriz[1][0]+otra.matriz[1][0], self.matriz[1][1]+otra.matriz[1][1]]]
+        return(matriz(nueva_matriz))
+    
+    #Defino resta
+    def __sub__(self, otra):
+        nueva_matriz = [ [self.matriz[0][0]-otra.matriz[0][0], self.matriz[0][1]-otra.matriz[0][1]], 
+                        [self.matriz[1][0]-otra.matriz[1][0], self.matriz[1][1]-otra.matriz[1][1]]]
+        return(matriz(nueva_matriz))
+    
+    #Defino multiplicación
+    def __mul__(self, numero):
+        nueva_matriz = [[self.matriz[0][0]*numero, self.matriz[0][1]*numero],
+                        [self.matriz[1][0]*numero, self.matriz[1][1]*numero]]
+        return(matriz(nueva_matriz))
+    
+    def __rmul__(self, numero):
+        nueva_matriz = [[numero*self.matriz[0][0], numero*self.matriz[0][1]],
+                        [numero*self.matriz[1][0], numero*self.matriz[1][1]]]
+        return(matriz(nueva_matriz))
+    
+    
 
 
+#Impresiones para matrices
+
+A = matriz([[1,2],[3,4]])
+B = matriz([[1,2],[3,4]])
+
+print(A)
+print()
+print(B)
+print()
+print(A+B)
+print()
+print(A-B)
+print()
+print(A*3)
+print()
+print(3*A)
 
 # impresiones 
-v1 = vector(1,2,3)
-v2 = vector(2,6,9)
-v3 = v1+v2
+#v1 = vector(1,2,3)
+#v2 = vector(2,6,9)
+#v3 = v1+v2
 
-print(v1)
-print(v2)
-print("numero entero:", 2)
-
-print("suma:",v3)
-print("resultado de la multiplicacion entre dos vectores (right):", v1*v2)
-print("resultado de la multiplicacion entre dos vectores:", v2*v1)
-print("resultado de la multiplicacion por un entero (right):", v2*2)
-print("resultado de la multiplicacion por un entero:", 2*v2)
+#print(v1)
+#print(v2)
+#print("numero entero:", 2)
+#print("suma:",v3)
+#print("resultado de la multiplicacion entre dos vectores (right):", v1*v2)
+#print("resultado de la multiplicacion entre dos vectores:", v2*v1)
+#print("resultado de la multiplicacion por un entero (right):", v2*2)
+#print("resultado de la multiplicacion por un entero:", 2*v2)
 
 
 
