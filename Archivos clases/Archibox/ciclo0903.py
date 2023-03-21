a8eee3019bf2afe7b69324e8345426e89247e1df
Juan Diego Arias <juand.ariasi@uqvirtual.edu.co>
2023-03-21 15:47:22 -0500

    comits al día

--- Archivos clases/Archibox/ciclo0903.py
+++ Archivos clases/Archibox/ciclo0903.py
@@ -22,13 +22,37 @@ N = int(input("¿Cuantos años dejará el dinero? "))
 p = float(input("ingrese el porcentaje de interés pactado (solo el numero): "))
 for i in range (N):
     x = x*(1*p/100)
-print("Despues de {0} años, usted tendrá {1}.format(N,x)")
+print("Despues de {0} años, usted tendrá {1}".format(N,x))
 
 #
-num = int(input("ingrese numero positivo"))
-while num<= 0:
+num = int(input("ingrese numero positivo: "))
+while num <= 0:
     num = int(input("POSITIVO: "))
     
 
 
-num = eval(input("Ingrese un número: "))
+# num = eval(input("Ingrese un número: "))
+
+#agrego commit
+
+
+
+while True:
+    temp = float(input("Ingrese una temperatura en Celcius para pasar a Fahrenheit (-1000 para salir): "))
+    if temp == -1000:
+        print("Bye")
+        break
+    print("Tu temperatura equivale a {}°F".format(9/5*temp+32))
+
+
+
+
+
+for i in range (10):
+    num = float(input("Ingrese un numero: "))
+    if num<0:
+        print("ERROR: Número negativo")
+        break 
+else:
+    print("El usuario introdujo los 10 valores")  
+          
---
+++ README_parcial.md
@@ -0,0 +1,9 @@
+# Mi primer Parcial
+## Programación Juan Diego Arias
+Universidad del Quindio `2023`
+- Physics
+```python
+print('Hola mundo')
+```
+cambio 2
+cambio GitHub!!
